9f792ed643b8dac2f73b1bfb5015ecf6ad11f328
Mattias Danielsson <matdanie@kth.se>
2015-04-22 23:43:22 +0200

    Added comments to most functions in app.d

--- source/app.d
+++ source/app.d
@@ -1,57 +1,75 @@
-import vibe.d;
 import std.regex;
 import std.stdio;
 import std.random : uniform;
-import vibe.stream.operations : readAllUTF8;
-import std.container.slist;
-
 import std.functional : memoize;
 import std.conv : to;
 import std.process : environment;
 import std.array : split, replace;
+import std.container.slist;
 
+import vibe.stream.operations : readAllUTF8;
+import vibe.d;
+
+/** 
+ * Global consts and vars.
+ */
 enum PRE_REGEX = ctRegex!("<pre>(.+?)</pre>", "s");
 enum LI_REGEX = ctRegex!("<li>(.+?)</li>", "s");
+
+const URL_BASE = "http://wiki.ceri.se/index.php?title=";
+
 string[] people;
 
+/**
+ * Complete representation of a quote.
+ */
 struct quote_response {
 	string quote;
 	string author;
 }
 
-string[] getMatching(R, RegEx)(const R url, RegEx r) {
+/**
+ * On each call, fetch the url and return all strings that matches the 
+ * regex.
+ */
+string[] fetchMatchingStringsFromUrl(R, RegEx)(const R url, RegEx regexp) {
 	auto response = requestHTTP(url);
+	auto response_str = response.bodyReader.readAllUTF8();
 
-	auto b = response.bodyReader.readAllUTF8();
+	string[] retval;
 
-	string[] quotes;
-
-
-	foreach(c; matchAll(b, r)) {
-		quotes ~= c[1].replace("\n", "<br>");
+	foreach(c; matchAll(response_str, regexp)) {
+		retval ~= c[1].replace("\n", "<br>");
 	}
 
-	return quotes;
+	return retval;
 }
 
+/**
+ * Fetches all quotes from the specified url.
+ */
 string[] getQuotesFor(const string url) {
-	return getMatching(url, PRE_REGEX);
+	return fetchMatchingStringsFromUrl(url, PRE_REGEX);
 }
 
 string[] getTidbitsFor(const string url) {
-	return getMatching(url, LI_REGEX);
+	return fetchMatchingStringsFromUrl(url, LI_REGEX);
 }
 
 alias fastGetQuotesFor = memoize!getQuotesFor;
 alias fastGetTidbitsFor = memoize!getTidbitsFor;
 
 /**
- * Read .rcpeople and return them.
+ * Read the config var
  */
 string[] getPeople() {
 	return environment["PEOPLE"].replace("\\","").split(",");
 }
 
+/**
+ * Pick a single quote from the supplied fetch function and return
+ * it in a quote_response
+ */
 quote_response _pick_one(string[] function(const(immutable(char)[])) fetch_function) {
 	if(people.length == 0) {
 		people = getPeople();
@@ -60,13 +78,16 @@ quote_response _pick_one(string[] function(const(immutable(char)[])) fetch_funct
 	quote_response response;
 
 	response.author = people[uniform(0, people.length)];
-	auto quotes = fetch_function("http://wiki.ceri.se/index.php?title=" ~ response.author);
+	auto quotes = fetch_function(URL_BASE ~ response.author);
 
 	response.quote = quotes.length > 0 ? quotes[uniform(0, quotes.length)] : "Nothing";
 
 	return response;
 }
 
+/*
+ * API
+ */
 void quote_api(HTTPServerRequest req, HTTPServerResponse res) {
 	res.writeJsonBody(_pick_one(&fastGetQuotesFor));
 }
@@ -75,6 +96,9 @@ void tidbit_api(HTTPServerRequest req, HTTPServerResponse res) {
 	res.writeJsonBody(_pick_one(&fastGetTidbitsFor));
 }
 
+/*
+ * VIEWS
+ */
 void hello(HTTPServerRequest req, HTTPServerResponse res) {
 	res.headers.addField("Cache-Control", "no-transform,public,max-age=300,s-maxage=900");
 	res.render!("index.dt");
